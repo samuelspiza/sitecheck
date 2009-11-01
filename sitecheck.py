d74de8c255eb23f9083fb890520c66910d191529
Samuel Spiza <samuelspiza@gmail.com>
2009-11-01 12:10:48 +0100

    missed rename in lassed commit

--- sitecheck.py
+++ sitecheck.py
@@ -16,24 +16,23 @@ siteslogedin = []
 logins = model.getLogins()
 tobesaved = []
 
+jar = cookielib.CookieJar()
+handler = urllib2.HTTPCookieProcessor(jar)
+opener = urllib2.build_opener(handler)
+urllib2.install_opener(opener)
+
 def checkSites():
     """ Start the site checking business. """
 
-    jar = cookielib.CookieJar()
-    handler = urllib2.HTTPCookieProcessor(jar)
-    opener = urllib2.build_opener(handler)
-    urllib2.install_opener(opener)
     
-    sites = model.getSites()
     recipients = dict([(r.name, r) for r in model.getRecipients()])
-    
+    sites = model.getSites()
     for site in sites:
-        diff = checkSiteDiff(site)
-        if diff is not None and 0 < len(diff.strip()):
+        site.run()
+        if site.mail != None:
             for recipient in site.to:
-                recipients[recipient].sitesWithDiff.append((site, diff.strip()))
-
-    print "Found " + str(sum([len(r.sitesWithDiff) for r in recipients.values()])) + " Sites with diffs"
+                recipients[recipient].sitesWithDiff.append(site)
+    print "Found " + str(sum([1 for s in sites if s.mail != None])) + " Sites with diffs"
     
     mails = [constructEmail(r) for r in recipients.values() if 0 < len(r.sitesWithDiff)]
     if 0 < len(mails):
@@ -46,12 +45,8 @@ def checkSiteDiff(site):
     """ Download the site and diff it with the old version (if available). """
     if site.login != None:
         login(site.login)
-    rawcontent = getResponse(site.url, None).read()
-    newcontent = sendmail.safe_unicode(BeautifulSoup(rawcontent).prettify())
-    newcontent = newcontent.encode('ascii', 'ignore')
-    if site.regex != None:
-        for regex in site.regex:
-            newcontent = re.sub(regex, '', newcontent)
+    
+    newcontent = getNormalizedContent(site)
     newlines = newcontent.split("\n")
     
     diff = None
@@ -65,11 +60,33 @@ def checkSiteDiff(site):
     
     return diff
 
+def login(name):
+    """ Performs a log in if not allready loged in. """
+    if name not in siteslogedin:
+        for login in logins:
+            if login.name == name:
+                for url in login.urls:
+                    if len(url) == 1:
+                        postData = None
+                    else:
+                        postData = dict([x.split("=") for x in url[1:]])
+                    login = getResponse(url[0], postData).read()
+                siteslogedin.append(name)
+
+def getNormalizedContent(site):
+    rawcontent = getResponse(site.url, None).read()
+	prettycontent = BeautifulSoup(rawcontent).prettify()
+    unicodecontent = sendmail.safe_unicode(prettycontent)
+    content = unicode.encode('ascii', 'ignore')
+    if site.regex != None:
+        for regex in site.regex:
+            content = re.sub(regex, '', content)
+	return content
+
 def getResponse(url, postData = None):
     header = { 'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
             'Accept-Language': 'de',
-            'Accept-Encoding': 'utf-8'                                   
-        } 
+            'Accept-Encoding': 'utf-8'} 
     if(postData is not None):
         postData = urllib.urlencode(postData)
     req = urllib2.Request(url, postData, header)
@@ -79,27 +96,17 @@ def getResponse(url, postData = None):
         print 'Error Code:', e.code
         return None
 
-def login(name):
-    """ Performs a log in if not allready loged in. """
-    if name not in siteslogedin:
-        for login in logins:
-            if login.name == name:
-                for url in login.urls:
-                    postData = dict([x.split("=") for x in url[1:]])
-                    login = getResponse(url[0], postData).read()
-                siteslogedin.append(name)
-
 def constructEmail(recipient):
     """ Construct the subject and body for the Email. """
     subject = "Observer Report - "
-    subject += ", ".join([site[0].name for site in recipient.sitesWithDiff])
+    subject += ", ".join([site.name for site in recipient.sitesWithDiff])
     subject += " " + strftime("%d.%m.%Y")
     
     body = "Observed Changes:\n"
     for site in recipient.sitesWithDiff:
-        body += "~"*10 + " " + site[0].name + " - " + site[0].url
+        body += "~"*10 + " " + site.name + " - " + site.url
         body += " " + "~"*10 + "\n"
-        body += site[1]
+        body += site.mail
         body += "\n\n"
     body += "SiteCheck.py - " + VERSION
     
